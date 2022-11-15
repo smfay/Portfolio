4b589096472dd42a6facd73b7b0be23286702e09
Sean Fay <saltycrackersagain@gmail.com>
2022-11-14 23:15:34 -0600

    Update Home.js

--- App/portfolio/src/screens/Home.js
+++ App/portfolio/src/screens/Home.js
@@ -24,7 +24,7 @@ const Home = () => {
                                 <h2 className='text-xl font-bold'>Matter</h2>
                                 <p className=''>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                             </header>
-                            <img src='./images/matter.png' className='w-full md:w-96' />
+                            <img src='./images/matter.png' alt="matter" className='w-full md:w-96' />
                         </div>
                     </div>
                 </section>
